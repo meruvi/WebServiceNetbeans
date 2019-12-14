8b96b7eb54798d7533ecbe8b53260518faf70844
meruvia <gmeruviarojas@gmail.com>
2019-12-14 17:53:39 -0400

    Creacion de servicio web

---
+++ src/java/web/ApplicationConfig.java
@@ -0,0 +1,29 @@
+package web;
+
+import java.util.Set;
+import javax.ws.rs.core.Application;
+
+/**
+ * @author Gmeruvia
+ */
+@javax.ws.rs.ApplicationPath("service")
+public class ApplicationConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<>();
+        addRestResourceClasses(resources);
+        return resources;
+    }
+
+    /**
+     * Do not modify addRestResourceClasses() method.
+     * It is automatically populated with
+     * all resources defined in the project.
+     * If required, comment out calling this method in getClasses().
+     */
+    private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(web.LoginService.class);
+    }
+    
+}
